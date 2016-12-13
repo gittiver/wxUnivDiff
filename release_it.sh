bb1aa7bbe95b0a8b93f3ee00f24ca1c0a1575552
Gulliver <gulliver@fargonauten.de>
2016-12-13 12:42:41 +0100

    version update and build script updated

--- release_it.sh
+++ release_it.sh
@@ -5,7 +5,7 @@ HOST_ARCH=`uname -m`
 STARTDIR=`pwd`
 
 CMAKE_BUILDDIR=build/$HOST_ARCH-$HOST_SYSTEM
-WXDIR=$STARTDIR/../wxWidgets/3.0.2
+WXDIR=$STARTDIR/../wxWidgets/3.1.0
 
 mkdir -p $CMAKE_BUILDDIR
 
@@ -13,12 +13,14 @@ cd $CMAKE_BUILDDIR
 
 cmake -DwxUnivDiff_WITH_DOCS:bool=ON \
 	  -DwxUnivDiff_WITH_TESTS:bool=ON \
-      -DCXXTEST_INCLUDE_DIR=3rdparty/cxxtest \
-      -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=3rdparty/cxxtest/bin/cxxtestgen \
+      -DCXXTEST_INCLUDE_DIR=$STARTDIR/3rdparty/cxxtest \
+      -DCXXTEST_PYTHON_TESTGEN_EXECUTABLE=$STARTDIR/3rdparty/cxxtest/bin/cxxtestgen \
       -DwxWidgets_CONFIG_EXECUTABLE:FILEPATH=$WXDIR/build-cocoa-debug/wx-config \
       ../..
 
-cmake --build . --target all --target test --target package 
+cmake --build . --target all
+cmake --build . --target test
+cmake --build . --target package 
 cmake --build . --target package_source
 cd $STARTDIR
 
