75018ecb21e3878c468a9549282c0854873ee6af
Gulliver <gulliver@fargonauten.de>
2016-12-21 17:47:05 +0100

    added rough version of commandline switch test

--- test/test_cmdline.hpp
+++ test/test_cmdline.hpp
@@ -1,15 +1,46 @@
 #include <cxxtest/TestSuite.h>
 #include "cmdline.hpp"
+#include <wx/cmdline.h>
 
 class CmdLineTestSuite : public CxxTest::TestSuite 
 {
 public:
+  wxCmdLineParser* parser; 
+  void setUp() 
+  {
+    parser = new wxCmdLineParser();
+  }
 
-   void setUp() 
-   {}
+  void tearDown()
+  {
+    delete parser;
+  }
 
    void testIsCmdLineSwitch()
    {
+     parser->SetDesc(getCmdlineEntryDesc());
+     wxString cmdLine("test -v");
+     parser->SetCmdLine(cmdLine);
+     parser->Parse();
+
+     TS_ASSERT_EQUALS(parser->FoundSwitch("v"),wxCMD_SWITCH_ON);
+
+#if defined WIN32
+
+     cmdLine = "test /v";
+     parser->SetCmdLine(cmdLine);
+     parser->Parse();
+
+     TS_ASSERT_EQUALS(parser->FoundSwitch("v"),wxCMD_SWITCH_ON);
+#endif
+
+     cmdLine = "test v";
+     parser->SetCmdLine(cmdLine);
+     parser->Parse();
+
+     TS_ASSERT_EQUALS(parser->FoundSwitch("v"),wxCMD_SWITCH_NOT_FOUND);
+     TS_ASSERT_EQUALS(parser->GetParam(1),"v");
+     
      //TS_ASSERT_EQUALS(isCmdLineSwitch("-v"),true);
      //TS_ASSERT_EQUALS(isCmdLineSwitch("-i"),true);
      //TS_ASSERT_EQUALS(isCmdLineSwitch("-l"),true);
